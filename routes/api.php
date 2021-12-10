9dd2927096f2ccd3e0fb58763d14e2924cf45303
Lalchand Sharma <lalchandsharma1206@gmail.com>
2021-12-10 16:52:25 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\API\CategoryController;
   |
  */
 
-Route::post('registers', [PassportAuthController::class, 'register']);
+Route::post('register', [PassportAuthController::class, 'register']);
 Route::post('login', [PassportAuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function ()
