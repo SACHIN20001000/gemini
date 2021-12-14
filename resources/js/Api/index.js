796977fbe8609b96b5891bb3ed223ee497dfd48c
Himanshu Kumar <himanshup118@gmail.com>
2021-12-14 12:06:57 +0530

    Update profile request code

---
+++ resources/js/Api/index.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const Api =  axios.create({
+  baseURL: `{process.env.VUE_APP_APIURL}`
+})
+
+Api.interceptors.request.use((config) => {
+    if (localStorage.getItem("token") !== null) {
+      config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+    }
+    return config
+  }, function (error) {
+    return Promise.reject(error)
+  })
+
+
+  Api.interceptors.response.use(function (response) {
+    return response
+  }, function (error) {
+    return Promise.reject(error)
+  })
+
+export default Api
