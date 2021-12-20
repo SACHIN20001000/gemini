ceb0cd5225b384c40cb78728d3bb15b0ef05bc88
Himanshu Kumar <himanshup118@gmail.com>
2021-12-20 18:54:04 +0530

    Update single category route

---
+++ resources/js/components/Category/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="main">
+    <p>Category</p>
+    <div v-if="category">
+      <img
+        :src="category.feature_image"
+        alt="category.name"
+      >
+      <p>{{category.name}}</p>
+    </div>
+  </div>
+</template>
+<style>
+  @import './profile.css';
+</style>
+<script>
+import {mapActions,mapGetters} from "vuex"
+
+export default {
+  name:"Category",
+  created(){
+    console.log('I ma here----')
+    console.log(this.$route.params.id)
+    if (this.$route.params.id) {
+      this.getCategory(this.$route.params.id)
+    }
+  },
+  computed: {
+    ...mapGetters(['category', 'catErrors'])
+  },
+  methods: {
+    ...mapActions(['getCategory'])
+  }
+}
+</script>
