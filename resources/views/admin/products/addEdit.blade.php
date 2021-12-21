d7baf53c0e3c1f49bd9d5c11d9f7ed3820bdf27e
Lalchand Sharma <lalchand.glocify@gmail.com>
2021-12-21 17:10:02 +0530

    Update addEdit.blade.php

--- resources/views/admin/products/addEdit.blade.php
+++ resources/views/admin/products/addEdit.blade.php
@@ -205,39 +205,6 @@
 </form>
                 <!-- form end  -->
 
-                <!-- ajax image form  -->
-
-                <form method="POST" enctype="multipart/form-data" id="upload_image_form" action="javascript:void(0)" >
-                  
-                
-                  <div class="row row-xs align-items-center mg-b-20">
-                                      <div class="col-md-4">
-                                          <label class="form-label mg-b-0">Add Media </label>
-                                      </div>
-                                      <div class="col-md-8 mg-t-5 mg-md-t-0">
-                                          <div class="row">
-                                              <div class="col-lg-12 col-md-12">
-                                                  <div class="card">
-                                                      <div class="card-body">
-
-
-                                                      <input type="file" name="images" placeholder="Choose image" id="image">
-
-                                                      </div>
-                                                  </div>
-                                              </div>
-                                          </div>
-                                      </div>
-                                  </div>
-                      
-                    <div class="col-md-12">
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </div>
-                    
-            </form>
-
-
-                <!-- ajax image form end  -->
             </div>
         </div>
     </div>
