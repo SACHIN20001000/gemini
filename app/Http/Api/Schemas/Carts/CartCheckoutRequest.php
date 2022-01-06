e371b45a7639bd7089e8dfa6ed6078ba11425259
upworkamrik <upworkamrik@gmail.com>
2022-01-06 15:56:21 +0530

    update code

--- app/Http/Api/Schemas/Carts/CartCheckoutRequest.php
+++ app/Http/Api/Schemas/Carts/CartCheckoutRequest.php
@@ -12,7 +12,7 @@ class CartCheckoutRequest
     /**
      * @OA\Property(
      *     title="key",
-     *     description="key  for storring",
+     *     description="key  for storing",
      *     example="e2e26459499dac9e6361419963e2e42861cc42adb8d6d",
      * )
      *
@@ -23,7 +23,7 @@ class CartCheckoutRequest
     /**
      * @OA\Property(
      *     title="name ",
-     *     description="name for storring",
+     *     description="name for storing",
      *     example="john",
      * )
      *
@@ -34,7 +34,7 @@ class CartCheckoutRequest
     /**
      * @OA\Property(
      *     title="email",
-     *     description="email for storring",
+     *     description="email for storing",
      *     example="john@gmail.com",
      * )
      *
@@ -45,7 +45,7 @@ class CartCheckoutRequest
       /**
      * @OA\Property(
      *     title="address",
-     *     description="address  for storring",
+     *     description="address  for storing",
      *     example="2",
      * )
      *
@@ -53,11 +53,21 @@ class CartCheckoutRequest
      */
     public $address;
 
+    /**
+     * @OA\Property(
+     *     title="phone",
+     *     description="phone  for storing",
+     *     example="2",
+     * )
+     *
+     * @var string
+     */
+    public $phone;
 
        /**
      * @OA\Property(
      *     title="zip_code",
-     *     description="zip_code  for storring",
+     *     description="zip_code  for storing",
      *     example="2",
      * )
      *
@@ -67,7 +77,7 @@ class CartCheckoutRequest
        /**
      * @OA\Property(
      *     title="city",
-     *     description="city  for storring",
+     *     description="city  for storing",
      *     example="2",
      * )
      *
@@ -77,7 +87,7 @@ class CartCheckoutRequest
        /**
      * @OA\Property(
      *     title="state",
-     *     description="state  for storring",
+     *     description="state  for storing",
      *     example="2",
      * )
      *
@@ -87,11 +97,132 @@ class CartCheckoutRequest
        /**
      * @OA\Property(
      *     title="country",
-     *     description="country  for storring",
+     *     description="country  for storing",
      *     example="2",
      * )
      *
      * @var string
      */
     public $country;
+
+    /**
+     * @OA\Property(
+     *     title="sh_email",
+     *     description="sh_email  for storing",
+     *     example="example@example.com",
+     * )
+     *
+     * @var string
+     */
+    public $sh_email;
+
+    /**
+     * @OA\Property(
+     *     title="sh_name",
+     *     description="sh_name  for storing",
+     *     example="Sam",
+     * )
+     *
+     * @var string
+     */
+    public $sh_name;
+
+    /**
+     * @OA\Property(
+     *     title="sh_city",
+     *     description="sh_city  for storing",
+     *     example="Chandigarh",
+     * )
+     *
+     * @var string
+     */
+    public $sh_city;
+
+    /**
+     * @OA\Property(
+     *     title="sh_state",
+     *     description="sh_state  for storing",
+     *     example="Chandigarh",
+     * )
+     *
+     * @var string
+     */
+    public $sh_state;
+
+    /**
+     * @OA\Property(
+     *     title="sh_address",
+     *     description="sh_address  for storing",
+     *     example="211 IT Park",
+     * )
+     *
+     * @var string
+     */
+    public $sh_address;
+
+    /**
+     * @OA\Property(
+     *     title="sh_country",
+     *     description="sh_country  for storing",
+     *     example="India",
+     * )
+     *
+     * @var string
+     */
+    public $sh_country;
+
+    /**
+     * @OA\Property(
+     *     title="sh_zip_code",
+     *     description="sh_zip_code  for storing",
+     *     example="134109",
+     * )
+     *
+     * @var string
+     */
+    public $sh_zip_code;
+
+    /**
+     * @OA\Property(
+     *     title="sh_phone",
+     *     description="sh_phone  for storing",
+     *     example="9854689758",
+     * )
+     *
+     * @var string
+     */
+    public $sh_phone;
+
+    /**
+     * @OA\Property(
+     *     title="payment_method",
+     *     description="payment_method  for storing",
+     *     example="Paypal",
+     * )
+     *
+     * @var string
+     */
+    public $payment_method;
+
+    /**
+     * @OA\Property(
+     *     title="shippingmethod",
+     *     description="shippingmethod  for storing",
+     *     example="express",
+     * )
+     *
+     * @var string
+     */
+    public $shippingmethod;
+
+    /**
+     * @OA\Property(
+     *     title="remark",
+     *     description="remark  for storing",
+     *     example="Test remark",
+     * )
+     *
+     * @var string
+     */
+    public $remark;
 }
