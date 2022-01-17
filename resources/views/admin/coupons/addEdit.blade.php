72dddcadda15cb388b31bcd18106f066385e0730
Lalchand Sharma <sachinkumar.glocify@gmail.com>
2022-01-17 18:22:12 +0530

    changes

--- resources/views/admin/coupons/addEdit.blade.php
+++ resources/views/admin/coupons/addEdit.blade.php
@@ -102,6 +102,8 @@ function addNumber() {
     // console.log(x)
     if(x === 'percentage'){
         $("input[type='number']").prop('max',100);
+    }else{
+        $("input[type='number']").removeAttr('max');
     }
 
 }
