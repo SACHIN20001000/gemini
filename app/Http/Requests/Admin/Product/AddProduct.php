beea5165445badec6e31d91f3be541f18376c1d8
Lalchand sharma <ls@morecoins.net>
2022-01-18 12:33:57 +0530

    Update AddProduct.php

--- app/Http/Requests/Admin/Product/AddProduct.php
+++ app/Http/Requests/Admin/Product/AddProduct.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests\Admin\Coupon;
+namespace App\Http\Requests\Admin\Product;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class AddCoupon extends FormRequest
+class AddProduct extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
