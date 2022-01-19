ff3f146d550fd471caddd315457c411be5da06e2
Lalchand Sharma <sachinkumar.glocify@gmail.com>
2022-01-19 13:53:33 +0530

    changes

--- resources/views/admin/products/addEdit.blade.php
+++ resources/views/admin/products/addEdit.blade.php
@@ -299,13 +299,10 @@
 <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
 <script type="text/javascript">
-   $('#hiddenDescription').summernote({
+  
+   $('textarea').summernote({
       height: 400
    });
-   $('#aboutDescription').summernote({
-      height: 400
-   });
-
    var productsEvent;
    (function() {
    var attributes =[];
