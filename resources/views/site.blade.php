0e394ec10701054e678ae4deae5edad48251580e
Himanshu Kumar <himanshup118@gmail.com>
2022-02-02 18:39:40 +0530

    add ssr functionality on vue

--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -1,27 +1,15 @@
-<html lang="en">
-
-<head>
-
+<html>
+    <head>
     <meta charset="UTF-8">
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <title>Vue SPA Demo</title>
+    <title>Vue SPA Demo Rending</title>
 	<script src="https://js.stripe.com/v3/"></script>
-</head>
-
-<body>
-    <div id="app">
-
-        <app></app>
-
-    </div>
-
-    <!-- <script src="{{ mix('js/app.js') }}"></script> -->
-     <script src="{{asset('js/app.js')}}" ></script>
-
-</body>
-
+    <script defer src="{{ mix('js/app-client.js') }}"></script>
+    </head>
+    <body>
+	{!! ssr('js/app-server.js')->fallback('<div id="app"></div>')->render() !!}
+    </body>
 </html>
