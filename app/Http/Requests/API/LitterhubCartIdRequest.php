fc1639114817421d0fad3241faea69b1ee6f3704
Lalchand Sharma <sachinkumar.glocify@gmail.com>
2022-02-14 12:18:20 +0530

    chnges

--- app/Http/Requests/API/LitterhubCartIdRequest.php
+++ app/Http/Requests/API/LitterhubCartIdRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\API;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class LitterhubCartRequest extends FormRequest
+class LitterhubCartIdRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
