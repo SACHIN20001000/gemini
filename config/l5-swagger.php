3b330ba2af8f1a4b40f050c4917e38dbdf49d98d
Lalchand Sharma <sachinkumar.glocify@gmail.com>
2022-03-08 10:20:07 +0530

    chnges

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -264,7 +264,7 @@ return [
          * Uncomment to add constants which can be used in annotations
          */
         'constants' => [
-        'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'http://3.132.243.209/api'),
+        'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'https://petparentsbranddev.tk/api/'),
         ],
         // STATIC TOKEN 
         'api_token' => [
